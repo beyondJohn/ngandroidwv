12084a3b43acd6504fbbd50994dc1b006189f111
beyondjohn <jp@beyondlogical.com>
2018-04-08 14:29:59 -0400

    stable android build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {APP_BASE_HREF} from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
 
-
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home'}
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'info',
+    component: HomeComponent,
+    data: { title: 'Info'}
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
